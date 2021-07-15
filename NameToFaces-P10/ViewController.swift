c04c4631835d6301de5b858af0daa56224c0bda9
Łukasz Nycz <lnycz96@gmail.com>
2021-07-15 19:14:18 +0200

    Initial Commit

---
+++ NameToFaces-P10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NameToFaces-P10
+//
+//  Created by Łukasz Nycz on 15/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
