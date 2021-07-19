86044bf1a87b9134348c9b47997f88ec77acf8b4
Łukasz Nycz <lnycz96@gmail.com>
2021-07-19 16:53:17 +0200

    Designing UICollectionView cells

---
+++ NameToFaces-P10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NameToFaces-P10
+//
+//  Created by Łukasz Nycz on 19/07/2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+}
